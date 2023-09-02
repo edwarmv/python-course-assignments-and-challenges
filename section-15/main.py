0cfc957a5f790ac15624aa3a6af28b5b7fea7e31
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-09-02 11:14:18 -0400

    feat(section-15): grabbing a class

--- section-15/main.py
+++ section-15/main.py
@@ -5,6 +5,15 @@ result = requests.get("https://example.com/")
 
 soup = bs4.BeautifulSoup(result.text, "lxml")
 
-selected_node = soup.select_one('head > title')
+selected_node = soup.select_one("head > title")
 
 print(selected_node and selected_node.text)
+
+# Grabbing a Class
+
+result = requests.get("https://en.wikipedia.org/wiki/Grace_Hopper")
+
+soup = bs4.BeautifulSoup(result.text, "lxml")
+
+for tag in soup.select(".vector-toc-text"):
+    print(tag.text)
