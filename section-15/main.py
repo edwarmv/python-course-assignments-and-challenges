1434a65ec393d75df9fbff7949896f4df555012f
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-09-02 11:51:10 -0400

    feat(section-15): grabbing an image

--- section-15/main.py
+++ section-15/main.py
@@ -1,5 +1,6 @@
 import requests
 import bs4
+import io
 
 result = requests.get("https://example.com/")
 
@@ -17,3 +18,18 @@ soup = bs4.BeautifulSoup(result.text, "lxml")
 
 for tag in soup.select(".vector-toc-text"):
     print(tag.text)
+
+# Grabbing an Image
+
+result = requests.get("https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)")
+
+soup = bs4.BeautifulSoup(result.text, "lxml")
+
+computer = soup.select_one(".infobox-image img")
+
+if computer:
+    image_link = f"https:{computer['src']}"
+    image = requests.get(image_link)
+    f = io.open("my_computer_image.jpg", "wb")
+    f.write(image.content)
+    f.close()
